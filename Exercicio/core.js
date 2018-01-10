25bc550ea9e8ce3ad3af84c97d497152941456b2
BRUNA VIEIRA TEIXEIRA <41126857858@jpmorgan.sp>
2018-01-10 21:49:56 -0200

    Exercicio JS

---
+++ Exercicio/core.js
@@ -0,0 +1,28 @@
+var produtos = [
+    {
+        image : "teste",
+        nome : "Puma Golf Tech Polo Tee"
+        // color : "15",
+        // size : "XXL",
+        // price : 72,
+        // total: 216
+    },
+
+    {
+        image : "teste",
+        nome : "Puma Golf Tech Polo Tee"
+        // color : "15",
+        // size : "XXL",
+        // price : 72,
+        // total: 216
+    }
+];
+
+
+
+
+
+
+
+//  document.getElementById('resultado').innerHTML = teste;
+ document.getElementById('product').innerHTML=produtos[0].image + " " ;
