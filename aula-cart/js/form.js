03452c03a0e473c1e33f55b5ca2f29a2eab91b56
BRUNA VIEIRA TEIXEIRA <41126857858@jpmorgan.sp>
2018-01-17 21:22:22 -0200

    Validação de Formularios

--- aula-cart/js/form.js
+++ aula-cart/js/form.js
@@ -16,7 +16,7 @@ function validarFormulario(){
 function validarEndereco(){
     event.preventDefault();
    
-    var retorno = false;
+    // var retorno = false;
     //gerar objeto endereco baseado nos dados do formulario
     objEndereco = {
         pais        : document.getElementById('selection_country').value,
@@ -32,79 +32,92 @@ function validarEndereco(){
     //     retorno = false;
     // }
 
-    if(objEndereco.pais > 0 &&  objEndereco.cidade > 0 && objEndereco.estado.trim() != '' && objEndereco.endereco1.trim()!='' && objEndereco.endereco2.trim() != ''){
-        retorno = true;
+    if (objEndereco.pais > 0 &&  objEndereco.cidade > 0 && objEndereco.estado.trim() != '' && objEndereco.endereco1.trim()!='' && objEndereco.endereco2.trim() != ''){
+        return  true;
+    } else {
+        return  false;
     }
 
-    // console.log (retorno);
 
-    // && objEndereco.endereco1.trim().lenght > 0 && objEndereco.endereco2.trim().lenght > 0 
-
-    console.log(retorno);
-    // if(objEndereco.pais > 0){
-    //     retorno = true;
-    // } else {
-    //     retorno = false;
+    // if (objEndereco.pais == 0){
+    //     return false;
+    // } if (objEndereco.cidade == 0){
+    //     return false;
+    // } if (objEndereco.estado.trim().length ==0 ){
+    //     return false;
+    // } if (objEndereco.endereco1.trim().length == 0){
+    //     return false;
+    // } if  (objEndereco.endereco2.trim() != ''){
+    //     return false;
+    // }
+    // else {
+    //     return true;
     // }
 
-    // if(objEndereco.cidade > 0){
-    //     retorno = true;
-    // } else {
-    //     retorno = false;
-    // }
+   
+    // console.log(retorno);
 
-
-    // if(objEndereco.estado.trim().lenght > 0){
-
-    //     retorno = true;
-    // } else {
-    //     retorno = false;
-    // }
-
-
-    // if(objEndereco.endereco1.trim().lenght > 0){
-
-    //     retorno = true;
-    // } else {
-    //     retorno = false;
-    // }
-
-
-    // if(objEndereco.endereco2.trim().lenght > 0){
-
-    //     retorno = true;
-    // } else {
-    //     retorno = false;
-    // }
-
-
-
-
-    
-
-
-    
-    //verificar se os dados estão preenchidos
-//     if( objEndereco.pais.trim().lenght == 0 ) {
-//         retorno = true;
-//         return retorno;
-    
-//     //retorno da função
-// }
 
 }
   
 
-function validarContato( _objContato ){
-
+function validarContato(){
+ var retorno = false;
+event.preventDefault();
 
     objContato = {
         nome        : document.getElementById('name').value,
         sobrenome   : document.getElementById('lastname').value,
         email       : document.getElementById('email').value,
-        tel         : document.getElementById('phone')
+        tel         : document.getElementById('phone').value
 
     };
+     
 
+    //  if (objContato.nome.trim() != '' && objContato.sobrenome.trim() != '' && objContato.email.trim() != '' && objContato.tel.trim() != ''){
+
+    //      return true;
+
+    //  } else {
+    //      return false;
+    //  }
+
+    if (objContato.nome.trim() != '' && objContato.sobrenome.trim() != '' && objContato.email.trim() != '' && objContato.tel.trim() != '' )
+    {
+         return true;
+
+     } else {
+         return false;
+     }
+
+    if(document.getElementById('s_input').checked){
+
+        var pacote = document.getElementById('s_input').value;
+    }
+    else if (document.getElementById('u_input').checked){
+
+        var pacote = document.getElementById('u_input').value;
+    }
+
+    else{
+        return false;
+    }
+
+    objContato = {
+        pacote : pacote,
+    };
+
+
+
+     if (objContato.pacote.value != '' ){
+         return true;
+
+     } else {
+         return false;
+     }
+
+
+    // }
+    
 }
 
